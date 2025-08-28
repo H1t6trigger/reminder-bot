102fbda673cc09898add4a822c79fccf4cb0702b
Alex <h1tclassique@vk.com>
2025-08-28 16:54:57 +0500

    chore: настройка безопасного хранения токена

--- config.py
+++ config.py
@@ -1,21 +1,27 @@
+import os
 import datetime
 import logging
 import sys
+from dotenv import load_dotenv
 
-TOKEN = '8427333691:AAF6tIBprUk3y48o86hYsWdcp8p-5LiQbPM'
+load_dotenv()
 
-#Настройки времени
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+if not TOKEN:
+    raise ValueError("TELEGRAM_BOT_TOKEN environment variable is not set")
+
 TIMEZONES = {"UTC": 0, "MSK": 1, "CHL": 3}
 OFFSET = datetime.timedelta(TIMEZONES['UTC'])
 TIMEZONE = datetime.timezone(OFFSET)
 
-#Настройки логирования
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('bot.log', mode='a'),
-        logging.StreamHandler(sys.stdout)   
+        logging.FileHandler('bot.log', mode='a'),  
+        logging.StreamHandler(sys.stdout)
     ],
     force=True
 )
+
+logging.info("Конфигурация загружена успешно")
